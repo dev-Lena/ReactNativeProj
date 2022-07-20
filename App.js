12ca00b1995543a4bc246ed46c158ce17fe9bb2f
keunnaLee <keunnalee@gmail.com>
2022-07-20 18:02:31 +0900

    feat: 앱 스플래시 화면 초기 구현

--- App.js
+++ App.js
@@ -1,18 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import * as SplashScreen from 'expo-splash-screen';
+import React, {useEffect, useState, useCallback} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Entypo from '@expo/vector-icons/Entypo';
+import * as Font from 'expo-font';
+import text from "react-native-web/dist/exports/Text";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-  return (
-      null
-  );  
-}
+  const [appIsReady, setAppIsReady] = useState(false);
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Pre-load fonts, make any API calls you need to do here
+        await Font.loadAsync(Entypo.font);
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience. Please remove this if you copy and paste the code!
+        await new Promise(resolve => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
+  return (
+      <View
+          style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+          onLayout={onLayoutRootView}>
+        <Text>SplashScreen Demo! 👋</Text>
+        <Entypo name="rocket" size={30} />
+      </View>
+  );
+}
