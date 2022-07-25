465ea3cf94869435e40cb9acd592068c06b81d7b
Keunna Lee <52783516+dev-Lena@users.noreply.github.com>
2022-07-25 12:38:24 +0900

    feat: 다크모드 전환에 따라 탭바, 헤더 관련 색상 업데이트 되도록 수정

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -3,15 +3,33 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Movies from "../Screens/Movies";
 import TV from "../Screens/TV";
 import Search from "../Screens/Search";
+import {Text, useColorScheme, View} from "react-native";
+import colors from "../colors";
 
 const Tab = createBottomTabNavigator();
 
-const Tabs = () => (
-        <Tab.Navigator>
+const Tabs = () => {
+    const isDark = useColorScheme() === "dark";
+    const scheme = useColorScheme();
+    return (
+        <Tab.Navigator screenOptions={{
+            tabBarActiveTintColor: colors(scheme).activeTint,
+            tabBarInactiveTintColor: colors(scheme).inactiveTint,
+            tabBarStyle: {
+                backgroundColor: colors(scheme).bgColor
+            },
+            headerStyle: {
+                backgroundColor: colors(scheme).bgColor
+            },
+            headerTitleStyle: {
+                color: colors(scheme).color
+            }
+        }}>
             <Tab.Screen name="Movies" component={Movies} />
             <Tab.Screen name="TV" component={TV} />
             <Tab.Screen name="Search" component={Search} />
         </Tab.Navigator>
-);
+    );
+};
 
 export default Tabs;
