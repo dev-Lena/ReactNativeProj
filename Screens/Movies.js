9710deee43789198344109c0832dc90782ffdf31
Keunna Lee <52783516+dev-Lena@users.noreply.github.com>
2022-07-25 16:04:44 +0900

    feat: Movies 화면 view component에 StyleSheet로 스타일 적용

--- Screens/Movies.js
+++ Screens/Movies.js
@@ -1,13 +1,23 @@
 import React from "react";
-import { TouchableOpacity, View, Text } from "react-native";
-import Tabs from "../navigation/Tabs";
+import {TouchableOpacity, View, Text, StyleSheet} from "react-native";
 
 const Movies = ( {navigation: { navigate } }) => (
     <TouchableOpacity
         onPress={() => navigate("Stack", {screen: "Three"})}
-        style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
-        <Text> Movies </Text>
+        style={styles.btn}>
+        <Text style={styles.text}> Movies </Text>
     </TouchableOpacity>
 );
 
 export default Movies;
+
+const styles = StyleSheet.create({
+    btn: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    text: {
+        color: "blue"
+    }
+});
