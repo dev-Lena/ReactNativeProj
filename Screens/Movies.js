2e0fbc648b574b58145dd1f664ee4e97560e08e3
Keunna Lee <52783516+dev-Lena@users.noreply.github.com>
2022-07-25 15:48:23 +0900

    feat: Tabs Navigator에 있는 Movies 탭 화면의 Text를 누르면 Stack Navigator의 Three 화면으로 이동

--- Screens/Movies.js
+++ Screens/Movies.js
@@ -1,11 +1,13 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { TouchableOpacity, View, Text } from "react-native";
 import Tabs from "../navigation/Tabs";
 
-const Movies = () => (
-    <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
-        <Text> Movie </Text>
-    </View>
+const Movies = ( {navigation: { navigate } }) => (
+    <TouchableOpacity
+        onPress={() => navigate("Stack", {screen: "Three"})}
+        style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
+        <Text> Movies </Text>
+    </TouchableOpacity>
 );
 
 export default Movies;
