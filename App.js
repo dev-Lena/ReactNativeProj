f64d70009ea1d897aeff6a4efe27d042e6c294a7
Keunna Lee <52783516+dev-Lena@users.noreply.github.com>
2022-07-25 11:27:52 +0900

    feat: Splash 화면 이후 첫 화면으로 탭 화면 보이도록 구현

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import * as SplashScreen from 'expo-splash-screen';
+import React from "react";
 import { StyleSheet, Text, View, Image } from 'react-native';
 import Entypo from '@expo/vector-icons/Entypo';
 import * as Font from 'expo-font';
-import {Ionicons} from "@expo/vector-icons"
+import { Ionicons } from "@expo/vector-icons"
 import { useAssets } from 'expo-asset';
+import { NavigationContainer } from '@react-navigation/native';
+import Tabs from "./navigation/Tabs";
+import { LogBox } from 'react-native';
+
+LogBox.ignoreLogs(['Require cycle:'])
 
 export default function App() {
     const [assets] = useAssets([require("./b9472222.png")]);
     const [loaded] = Font.useFonts(Ionicons.font);
+
     if (!assets || !loaded) {
         return (
             <View
@@ -19,16 +26,9 @@ export default function App() {
             );
     }
     return (
-        <View
-            style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-            <Text>SplashScreen Demo~~~! 👋</Text>
-            <Entypo name="rocket" size={30}/>
-            <Image style={styles.logo}
-                   source={{
-                       uri: 'https://upload.wikimedia.org/wikipedia/en/thumb/3/3b/SpongeBob_SquarePants_character.svg/640px-SpongeBob_SquarePants_character.svg.png',
-                   }}
-            />
-        </View>
+        <NavigationContainer>
+            <Tabs/>
+        </NavigationContainer>
     );
 }
 
