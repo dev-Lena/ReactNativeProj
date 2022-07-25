fe23beea1658497a7211350c2a05fb530d6e028d
Keunna Lee <52783516+dev-Lena@users.noreply.github.com>
2022-07-25 15:46:13 +0900

    feat: 화면을 쌓는 구조의 Stack Navigator 구현

---
+++ navigation/Stack.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Text, View, TouchableOpacity } from "react-native";
+
+const ScreenOne = ({ navigation: { navigate } }) => (
+    <TouchableOpacity onPress={() => navigate("Two")}>
+        <Text>Go To Two</Text>
+    </TouchableOpacity>
+);
+const ScreenTwo = ({ navigation: { navigate } }) => (
+    <TouchableOpacity onPress={() => navigate("Three")}>
+        <Text>Go To Three</Text>
+    </TouchableOpacity>
+);
+const ScreenThree = ({ navigation: { navigate } }) => (
+    <TouchableOpacity onPress={() => navigate("Tabs", { screen: "Search" })}>
+        <Text>Go To Search Tab</Text>
+    </TouchableOpacity>
+);
+
+const NativeStack = createNativeStackNavigator();
+
+const Stack = () => <NativeStack.Navigator
+    screenOptions={{
+        headerBackTitleVisible: false
+    }}>
+    <NativeStack.Screen name="One" component={ScreenOne} />
+    <NativeStack.Screen name="Two" component={ScreenTwo} />
+    <NativeStack.Screen name="Three" component={ScreenThree} />
+</NativeStack.Navigator>
+
+export  default  Stack;
