b50a68691446bdabfa538c3fb9c261fa15816969
Keunna Lee <52783516+dev-Lena@users.noreply.github.com>
2022-07-25 16:06:06 +0900

    feat: view 컴포넌트에 StyleSheet -> styled-components로 스타일 적용 방식 변경

--- Screens/Movies.js
+++ Screens/Movies.js
@@ -1,12 +1,22 @@
 import React from "react";
+import styled from "styled-components/native";
 import {TouchableOpacity, View, Text, StyleSheet} from "react-native";
 
+const Btn = styled.View`
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+`;
+
+const Title = styled.Text`
+    color: blue;
+`;
+
 const Movies = ( {navigation: { navigate } }) => (
-    <TouchableOpacity
-        onPress={() => navigate("Stack", {screen: "Three"})}
-        style={styles.btn}>
-        <Text style={styles.text}> Movies </Text>
-    </TouchableOpacity>
+    <Btn
+        onPress={() => navigate("Stack", {screen: "Three"})}>
+        <Title> Movies </Title>
+    </Btn>
 );
 
 export default Movies;
