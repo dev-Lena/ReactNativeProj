dc0efb197b1283664cff62c226ecc2d6ee7c0b41
Keunna Lee <52783516+dev-Lena@users.noreply.github.com>
2022-07-26 17:45:41 +0900

    feat: 로딩 액티비티 인디케이터, 현재 상영 영화 캐러셀 (히어로), trending 영화 수평 스크롤 구현

--- Screens/Movies.tsx
+++ Screens/Movies.tsx
@@ -1,23 +1,133 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import styled from "styled-components/native";
+import {ActivityIndicator, Dimensions, ScrollView} from "react-native";
 import {NativeStackScreenProps} from "@react-navigation/native-stack";
+import Swiper from 'react-native-swiper';
+import Slide from "../components/Slides";
+import Poster from "../components/Poster";
+const API_KEY = ''
 
-const Btn = styled.TouchableOpacity`
+const Container = styled.ScrollView`
+`;
+
+const Loader = styled.View`
     flex: 1;
     justify-content: center;
     align-items: center;
-    background-color: ${(props) => props.theme.mainBgColor};
+ `;
+
+const { height: SCREEN_HEIGHT } = Dimensions.get("window");
+
+const ListTitle = styled.Text`
+    color: white;
+    font-size: 18px;
+    font-weight: 600;
+    margin-left: 30px;
+`;
+
+const TrendingScroll = styled.ScrollView`
+  margin-top: 20px;
+`;
+
+const Movie = styled.View`
+  margin-right: 10px;
+  align-items: center;
 `;
 
 const Title = styled.Text`
-    color: ${(props) => props.theme.textColor};
+  color: white;
+  font-weight: 600;
+  margin-top: 7px;
+  margin-bottom: 5px;
+`;
+const Votes = styled.Text`
+  color: rgba(255, 255, 255, 0.8);
+  font-size: 10px;
 `;
 
-const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = ( {navigation: { navigate },
-}) => (
-    <Btn onPress={() => navigate("Stack", { screen: "Three" })}>
-        <Title>Movies</Title>
-    </Btn>
-);
+const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
+    const [loading, setLoading] = useState(true);
+    const [nowPlaying, setNowPlaying] = useState([]);
+    const [upcoming, setUpcoming] = useState([]);
+    const [trending, setTrending] = useState([]);
+    const getTrending = async() => {
+        const { results } = await (
+            await fetch(
+                `https://api.themoviedb.org/3/trending/movie/week?api_key=${API_KEY}`
+            )
+        ).json();
+        setTrending(results);
+    }
+    const getUpcoming = async () => {
+        const { results } = await (
+            await fetch(
+                `https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1&region=KR`
+            )
+        ).json();
+        setUpcoming(results);
+    };
+    const getNowPlaying = async () => {
+        const { results } = await (
+            await fetch(
+            `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1&region=KR`
+            )
+        ).json();
+        setNowPlaying(results);
+    };
+    const getData = async () => {
+        await Promise.all([getTrending(), getUpcoming(), getNowPlaying()]);
+    }
+    useEffect(() => {
+        getData();
+        setLoading(false);
+    }, []);
+    return loading ? (
+        <Loader>
+            <ActivityIndicator size="small" />
+        </Loader>
+    ) : (<Container>
+        <Swiper
+            horizontal
+            loop
+            autoplay
+            autoplayTimeout={2}
+            showsButtons={false}
+            showsPagination={false}
+            containerStyle={{
+                marginBottom: 30,
+                width: "100%",
+                height: SCREEN_HEIGHT / 4
+            }}
+        >
+            {nowPlaying.map((movie) => (
+                <Slide
+                    key={movie.id}
+                    backdropPath={movie.backdrop_path}
+                    posterPath={movie.poster_path}
+                    originalTitle={movie.original_title}
+                    voteAverage={movie.vote_average}
+                    overview={movie.overview}
+                />
+            ))}
+        </Swiper>
+            <ListTitle>Trending Movies</ListTitle>
+            <TrendingScroll
+                contentContainerStyle={{ paddingLeft: 30 }}
+                horizontal
+                showsHorizontalScrollIndicator={false}
+            >
+                {trending.map(movie =>
+                    <Movie key={movie.id}>
+                        <Poster path={movie.poster_path} />
+                        <Title>
+                            {movie.original_title.slice(0.13)}
+                            {movie.original_title.length > 13 ? "..." : null}
+                        </Title>
+                        <Votes>⭐️ {movie.vote_average}/10</Votes>
+                    </Movie>)}
+            </TrendingScroll>
+    </Container>
+    );
+};
 
 export default Movies;
