d86e47a8e178c34bba150268d973c4afd61cc4f2
keunnaLee <keunnalee@gmail.com>
2022-07-27 15:57:18 +0900

    feat: 곧 개봉 영화 섹션 구현 및 component 분리

--- Screens/Movies.tsx
+++ Screens/Movies.tsx
@@ -45,6 +45,36 @@ const Votes = styled.Text`
   font-size: 10px;
 `;
 
+const ListContainer = styled.View`
+   margin-bottom: 40px;
+ `;
+
+const HMovie = styled.View`
+   padding: 0px 30px;
+   margin-bottom: 30px;
+   flex-direction: row;
+ `;
+
+const HColumn = styled.View`
+   margin-left: 15px;
+   width: 80%;
+ `;
+
+const Overview = styled.Text`
+   color: white;
+   opacity: 0.8;
+   width: 80%;
+ `;
+
+const Release = styled.Text`
+   color: white;
+   font-size: 12px;
+   margin-vertical: 10px;
+ `;
+const ComingSoonTitle = styled(ListTitle)`
+   margin-bottom: 30px;
+ `;
+
 const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
     const [loading, setLoading] = useState(true);
     const [nowPlaying, setNowPlaying] = useState([]);
@@ -110,6 +140,7 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
                 />
             ))}
         </Swiper>
+            <ListContainer>
             <ListTitle>Trending Movies</ListTitle>
             <TrendingScroll
                 contentContainerStyle={{ paddingLeft: 30 }}
@@ -123,9 +154,30 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
                             {movie.original_title.slice(0.13)}
                             {movie.original_title.length > 13 ? "..." : null}
                         </Title>
-                        <Votes>⭐️ {movie.vote_average}/10</Votes>
+                        <Votes>
+                            {movie.vote_average > 0
+                                ? `⭐️ ${movie.vote_average}/10`
+                                : `Coming soon`}
+                        </Votes>
                     </Movie>)}
             </TrendingScroll>
+            </ListContainer>
+            <ComingSoonTitle>Coming soon</ComingSoonTitle>
+            {upcoming.map(movie =>
+                <HMovie key={movie.id}>
+                    <Poster path={movie.poster_path} />
+                    <HColumn>
+                        <Title>{movie.original_title}</Title>
+                        <Release>
+                            {new Date(movie.release_date).toLocaleDateString("ko")}
+                        </Release>
+                        <Overview>{movie.overview !== "" && movie.overview.length > 80
+                            ? `${movie.overview.slice(0, 140)}...`
+                            : movie.overview}
+                        </Overview>
+                    </HColumn>
+                </HMovie>
+            )}
     </Container>
     );
 };
