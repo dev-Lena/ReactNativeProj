86e9ff19f73adea94f7f955f4eca5df003d7c0c3
keunnaLee <keunnalee@gmail.com>
2022-07-27 17:43:08 +0900

    feat: 다른 탭에서 재사용할 수 있도록 Component 분리

--- Screens/Movies.tsx
+++ Screens/Movies.tsx
@@ -6,6 +6,8 @@ import Swiper from 'react-native-swiper';
 import Slide from "../components/Slides";
 import Poster from "../components/Poster";
 const API_KEY = ''
+import VMedia from "../components/VMedia";
+import HMedia from "../components/HMedia";
 
 const Container = styled.ScrollView`
 `;
@@ -23,56 +25,19 @@ const ListTitle = styled.Text`
     font-size: 18px;
     font-weight: 600;
     margin-left: 30px;
+    margin-bottom: 20px;
 `;
 
 const TrendingScroll = styled.ScrollView`
   margin-top: 20px;
 `;
 
-const Movie = styled.View`
-  margin-right: 10px;
-  align-items: center;
-`;
-
-const Title = styled.Text`
-  color: white;
-  font-weight: 600;
-  margin-top: 7px;
-  margin-bottom: 5px;
-`;
-const Votes = styled.Text`
-  color: rgba(255, 255, 255, 0.8);
-  font-size: 10px;
-`;
-
 const ListContainer = styled.View`
    margin-bottom: 40px;
  `;
 
-const HMovie = styled.View`
-   padding: 0px 30px;
-   margin-bottom: 30px;
-   flex-direction: row;
- `;
-
-const HColumn = styled.View`
-   margin-left: 15px;
-   width: 80%;
- `;
-
-const Overview = styled.Text`
-   color: white;
-   opacity: 0.8;
-   width: 80%;
- `;
-
-const Release = styled.Text`
-   color: white;
-   font-size: 12px;
-   margin-vertical: 10px;
- `;
 const ComingSoonTitle = styled(ListTitle)`
-   margin-bottom: 30px;
+   margin-bottom: 20px;
  `;
 
 const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
@@ -173,21 +138,15 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
             </TrendingScroll>
             </ListContainer>
             <ComingSoonTitle>Coming soon</ComingSoonTitle>
-            {upcoming.map(movie =>
-                <HMovie key={movie.id}>
-                    <Poster path={movie.poster_path} />
-                    <HColumn>
-                        <Title>{movie.original_title}</Title>
-                        <Release>
-                            {new Date(movie.release_date).toLocaleDateString("ko")}
-                        </Release>
-                        <Overview>{movie.overview !== "" && movie.overview.length > 80
-                            ? `${movie.overview.slice(0, 140)}...`
-                            : movie.overview}
-                        </Overview>
-                    </HColumn>
-                </HMovie>
-            )}
+            {upcoming.map((movie) => (
+                <HMedia
+                    key={movie.id}
+                    posterPath={movie.poster_path}
+                    originalTitle={movie.original_title}
+                    overview={movie.overview}
+                    releaseDate={movie.release_date}
+                />
+            ))}
     </Container>
     );
 };
