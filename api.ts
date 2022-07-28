5e003b9678943ab98e34bbfbc9e562d9c9056d71
Keunna Lee <52783516+dev-Lena@users.noreply.github.com>
2022-07-28 14:45:46 +0900

    feat: API path 및 Data 디코딩 로직 별도 파일로 분리

---
+++ api.ts
@@ -0,0 +1,16 @@
+const API_KEY = '';
+const BASE_URL = 'https://api.themoviedb.org/3';
+
+const trending = () =>
+    fetch(`${BASE_URL}/trending/movie/week?api_key=${API_KEY}`
+    ).then(res => res.json());
+
+const upcoming = () =>
+    fetch(`${BASE_URL}/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1&region=KR`
+    ).then(res => res.json());
+
+const nowPlaying = () =>
+    fetch(`${BASE_URL}/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1&region=KR`
+    ).then(res => res.json());
+
+export const moviesAPI = { trending, upcoming, nowPlaying };
