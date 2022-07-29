5e0848e2574d88684ea916625dd0ad2dce6720b0
keunnaLee <keunnalee@gmail.com>
2022-07-29 17:18:36 +0900

    feat: TVs 탭 화면 useQuery로 API fetch 후 UI 표시 구현

--- Screens/TV.tsx
+++ Screens/TV.tsx
@@ -1,7 +1,24 @@
 import React from "react";
 import {ScrollView} from "react-native";
+import {useQuery} from "@tanstack/react-query";
+import {tvAPI} from "../api";
+import Loader from "../components/Loader";
 import HList from "../components/HList";
 
 const TV = () => {
+    const {isLoading: todayLoading, data: todayData} = useQuery(['tv', 'today'], tvAPI.airingToday)
+    const {isLoading: trendingLoading, data: trendingData} = useQuery(['tv', 'trending'], tvAPI.trending)
+    const {isLoading: topLoading, data: topData} = useQuery(['tv', 'top'], tvAPI.topRated)
+    const loading = todayLoading || topLoading || trendingLoading;
+    if (loading) {
+        return  <Loader />;
+    }
+    return (
+        <ScrollView contentContainerStyle={{ paddingVertical: 30}}>
+            <HList title="Trending TV" data={trendingData.results}/>
+            <HList title="Airing Today" data={todayData.results}/>
+            <HList title="Top Rated TV" data={topData.results}/>
+        </ScrollView>
+  );
 };
 
