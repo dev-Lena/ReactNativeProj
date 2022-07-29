56f204a98aaf76705041e83407e751249aa66bb9
keunnaLee <keunnalee@gmail.com>
2022-07-29 18:30:35 +0900

    feat: TVs 탭 화면 useQuery로 API fetch 후 UI 및 새로고침 구현

--- Screens/TV.tsx
+++ Screens/TV.tsx
@@ -1,20 +1,48 @@
 import React from "react";
-import {ScrollView} from "react-native";
-import {useQuery} from "@tanstack/react-query";
+import {RefreshControl, ScrollView} from "react-native";
+import {useQuery, useQueryClient} from "@tanstack/react-query";
 import {tvAPI} from "../api";
 import Loader from "../components/Loader";
 import HList from "../components/HList";
+import Movies from "./Movies";
 
 const TV = () => {
-    const {isLoading: todayLoading, data: todayData} = useQuery(['tv', 'today'], tvAPI.airingToday)
-    const {isLoading: trendingLoading, data: trendingData} = useQuery(['tv', 'trending'], tvAPI.trending)
-    const {isLoading: topLoading, data: topData} = useQuery(['tv', 'top'], tvAPI.topRated)
+    const queryClient = useQueryClient();
+    const {isLoading: todayLoading,
+        data: todayData,
+        isRefetching: todayRefetching
+    } = useQuery(
+        ['tv', 'today'],
+         tvAPI.airingToday
+    );
+    const {isLoading: trendingLoading,
+        data: trendingData,
+        isRefetching: trendingRefetching
+    } = useQuery(
+        ['tv', 'trending'],
+        tvAPI.trending
+    );
+    const {isLoading: topLoading,
+        data: topData,
+        isRefetching: topRefetching
+    } = useQuery(
+        ['tv', 'top'],
+        tvAPI.topRated
+    );
+    const onRefresh = () => {
+        queryClient.refetchQueries(['tv']);
+    }
     const loading = todayLoading || topLoading || trendingLoading;
+    const refreshing = todayRefetching || trendingRefetching || topRefetching;
     if (loading) {
         return  <Loader />;
     }
     return (
-        <ScrollView contentContainerStyle={{ paddingVertical: 30}}>
+        <ScrollView
+            refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={onRefresh}/>
+            }
+            contentContainerStyle={{ paddingVertical: 30}}>
             <HList title="Trending TV" data={trendingData.results}/>
             <HList title="Airing Today" data={todayData.results}/>
             <HList title="Top Rated TV" data={topData.results}/>
@@ -22,3 +50,4 @@ const TV = () => {
   );
 };
 
+export default TV;
