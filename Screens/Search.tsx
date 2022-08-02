fe9975d52b8d8c627b3c311dff415b9c8b6e9a8d
Keunna Lee <52783516+dev-Lena@users.noreply.github.com>
2022-08-02 11:52:52 +0900

    feat: Search 탭 검색바 기본 구현

--- Screens/Search.tsx
+++ Screens/Search.tsx
@@ -1,22 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 import { View, Text } from "react-native";
 import styled from "styled-components/native";
 
-const Btn = styled.TouchableOpacity`
-    flex: 1;
-    justify-content: center;
-    align-items: center;
-    background-color: ${props => props.theme.mainBgColor};
+const Container = styled.ScrollView``;
+
+const SearchBar = styled.TextInput`
+    background-color: white;
+    padding: 10px 15px;
+    border-radius: 15px;
+    width: 90%;
+    margin: 10px auto;
 `;
 
-const Title = styled.Text`
-    color: ${props => props.theme.textColor};
-`;
-
-const Search = () => (
-    <Btn>
-        <Title> Search </Title>
-    </Btn>
-);
+const Search = () => {
+    const [query, setQuery] = useState("");
+    const onChangeText = (text: string) => setQuery(text);
+    return (
+        <Container>
+            <SearchBar
+                placeholder="Search for Movie or TV Show"
+                placeholderTextColor="grey"
+                returnKeyType="search"
+                onChangeText={onChangeText}
+            />
+        </Container>
+    );
+};
 
 export default Search;
