b9d0508c5fb41b3f0c459bbf6670c0ae9092da89
keunnaLee <keunnalee@gmail.com>
2022-08-08 10:48:53 +0900

    feat: Detail 화면 네비게이션 헤더 스타일 구현

--- navigation/Stack.js
+++ navigation/Stack.js
@@ -1,14 +1,25 @@
 import React from "react";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Detail from "../Screens/Detail";
+import colors from "../colors";
 
 const NativeStack = createNativeStackNavigator();
 
-const Stack = () => <NativeStack.Navigator
-    screenOptions={{
-        headerBackTitleVisible: false
-    }}>
-    <NativeStack.Screen name="Detail" component={Detail} />
-</NativeStack.Navigator>
+const Stack = () => {
+    return (
+        <NativeStack.Navigator
+            screenOptions={{
+               headerBackTitleVisible: false,
+               headerStyle: {
+                   backgroundColor: colors(scheme).bgColor
+                },
+               headerTitleStyle: {
+                   color: colors(scheme).color
+               },
+          }}>
+            <NativeStack.Screen name="Detail" component={Detail}/>
+        </NativeStack.Navigator>
+    );
+}
 
 export  default  Stack;
