a8f5c4ea6e9c14d2602358c7a41e269d48c01765
keunnaLee <keunnalee@gmail.com>
2022-08-08 12:26:30 +0900

    feat: 이전 화면에서 전달받은 데이터를 Detail 화면에 먼저 표시하도록 구현

--- Screens/Detail.tsx
+++ Screens/Detail.tsx
@@ -1,13 +1,46 @@
 import React, {useEffect} from "react";
 import styled from "styled-components";
-import {ImageBackground} from "react-native";
+import {LinearGradient} from "expo-linear-gradient";
+import {Dimensions, ImageBackground, StyleSheet, useColorScheme} from "react-native";
 import {NativeStackScreenProps} from "@react-navigation/native-stack";
 import {Movie} from "../api";
 import TV from "./TV";
 import Poster from "../components/Poster";
+import {makeImgPath} from "../utils";
+import colors from "../colors";
+
+const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 
 const Container = styled.ScrollView`
     background-color: ${(props) => props.theme.mainBgColor};
+`;
+
+const Header = styled.View`
+    height: ${SCREEN_HEIGHT/4}px;
+    justify-content: flex-end;
+    padding: 0px 20px;
+`;
+
+const Background = styled.Image`
+`;
+
+const Column = styled.View`
+    flex-direction: row;
+    width: 80%;
+`;
+
+const Title = styled.Text`
+    color: white;
+    font-size: 36px;
+    align-self: flex-end;
+    margin-left: 15px;
+    font-weight: 500
+`;
+
+const Overview = styled.Text`
+    color: ${(props) => props.theme.textColor};
+    margin-top: 20px;
+    padding: 0px 20px;
 `;
 
 type RootStackParamList = {
@@ -23,13 +56,33 @@ const Detail: React.FC<DetailScreenProps> = ({
     useEffect(() => {
         setOptions({
             title: 'original_title' in params
-                ? params.original_title
-                : params.original_name,
+                ? "Movie"
+                : "TV Show",
         });
     }, []);
+    const scheme = useColorScheme();
     return (
         <Container>
-            <Poster path={params.poster_path || ""}/>
+            <Header>
+                <Background
+                    style={StyleSheet.absoluteFill}
+                    source={{uri: makeImgPath(params.backdrop_path || "")}}
+                />
+                <LinearGradient
+                    colors={["transparent", colors(scheme).bgColor]}
+                    style={StyleSheet.absoluteFill}
+                />
+                <Column>
+                    <Poster path={params.poster_path || ""}/>
+                    <Title>
+                        {'original_title' in params
+                            ? params.original_title
+                            : params.original_name
+                        }
+                    </Title>
+                </Column>
+            </Header>
+            <Overview>{params.overview}</Overview>
         </Container>
     )
 };
