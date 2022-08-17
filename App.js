ec2c6daa6f0bd88007b8c4845d7ff7babe9fa47f
Keunna Lee <52783516+dev-Lena@users.noreply.github.com>
2022-08-17 12:28:22 +0900

    feat: withTiming 애니메이션

--- App.js
+++ App.js
@@ -1,21 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, SafeAreaView, StyleSheet, View } from 'react-native';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+} from 'react-native-reanimated';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+const App = () => {
+  const x = useSharedValue(0);
+
+  // 애니메이션 효과를 주는 style 의 경우 useAnimatedStyle Hook 을 이용하여 따로 정의합니다.
+  const animatedStyle = useAnimatedStyle(
+      () => ({
+        transform: [{ translateX: x.value }],
+      }),
+      [],
   );
-}
+
+  return (
+      <SafeAreaView style={styles.view}>
+        <View style={styles.rectView}>
+          <Animated.View style={[styles.rect, animatedStyle]} />
+        </View>
+
+        <View style={styles.button}>
+          <Button
+              title="이동하기"
+              // 버튼을 누르면 랜덤한 곳으로 이동합니다.
+              onPress={() => (x.value = Math.random() * 255)}
+          />
+        </View>
+      </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  view: {
+    marginVertical: 300,
+    marginHorizontal: 50,
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  rectView: {
+    width: '100%',
+  },
+  rect: {
+    width: 80,
+    height: 80,
+    backgroundColor: '#001a90',
+    borderRadius: 10,
+  },
+  button: {
+    padding: 20,
   },
 });
+
+export default App;
